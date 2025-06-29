3766e253ac07b631ee5660f18e46bc2ed6b9192e
mpr <marinpericradan1@gmail.com>
2025-06-29 12:44:14 +0200

    feat: basic fepl logic added

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,28 @@
+import { createInterface } from "readline"
+
 export function cleanInput(input: string): Array<string> {
 	return input.trim().toLowerCase().split(" ").filter((word) => word !== "")
+}
+
+
+const readInput = createInterface(
+	{
+		input: process.stdin,
+		output: process.stdout,
+		prompt: "Pokedex > ",
+	}
+)
+
+export function startREPL() {
+	readInput.prompt()
+	readInput.on("line", (input) => {
+		const cleanedInput = cleanInput(input)
+
+		if (!cleanedInput.length) {
+			readInput.prompt()
+		} else {
+			console.log(`Your command was: ${cleanedInput[0]}`)
+			readInput.prompt()
+		}
+	})
 }
